a46a4253e1220a79a49c4e6487a91258e528b783
Milagro Scardigno <mdscardigno@gmail.com>
2021-11-04 14:00:38 -0400

    displaying the highest number the user entered

--- Program.cs
+++ Program.cs
@@ -2,11 +2,24 @@
 
 namespace HigherNumber
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to HigherNumber");
+            int number = 0;
+            int higherNumber = int.MinValue; //smallest possible int like -300 million
+            do
+            {
+                Console.WriteLine("Please enter a number: ");
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number > higherNumber)
+                {
+                    higherNumber = number;
+                }
+            } while (number != 0);
+            Console.WriteLine("From all the numbers that you have been entered, the higher number is:" + higherNumber);
+            Console.ReadKey();
+        }
     }
-  }
 }
